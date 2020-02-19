ec5266a902459f4238f4e7cce16515a44fde5502
Jens Rene Suckert <JensRene.Suckert@nttdata.com>
2020-02-19 15:05:56 +0000

    Fix tests

--- main_test.go
+++ main_test.go
@@ -21,7 +21,7 @@ func TestSayHello(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
 
-	expected := "Hello, {{name}}\n"
+	expected := "Hello {{name}}\n"
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
